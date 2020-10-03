407377b14e48e6037b84bfc2888a885c4bd32b7a
yasirdin <yasir-360@hotmail.co.uk>
2020-10-03 15:40:32 +0100

    Minor updates to comment in setup.bash

--- setup.bash
+++ setup.bash
@@ -2,17 +2,17 @@
 
 set -euo pipefail
 
-# overwrite dotfiles in $HOME and soft symbolic link
+# Overwrite dotfiles in $HOME and soft symbolic link
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
-# install oh-my-zsh
+# Install oh-my-zsh
 if [ ! -d ~/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
-# vundle and plugin installation
+# Vundle and plugin installation
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
