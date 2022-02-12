18193d64ca86519b21f13b566cbe2803ef3f6119
yasirdin <yasir@yasirdin.net>
2022-02-12 17:18:24 +0000

    add nvimtree

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -4,4 +4,12 @@ return require('packer').startup(function()
   use 'airblade/vim-gitgutter'
 
   use 'ishan9299/nvim-solarized-lua'
+
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons',
+    },
+    config = function() require'nvim-tree'.setup {} end
+  }
 end)
