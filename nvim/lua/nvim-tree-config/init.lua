95e62b71d2ac7ef7e6a1975c44538c7afcd135e5
Yasir Din <yasir@yasirdin.net>
2024-03-21 19:42:43 +0000

    nerdtree config and tmux config

--- nvim/lua/nvim-tree-config/init.lua
+++ nvim/lua/nvim-tree-config/init.lua
@@ -1,16 +1,16 @@
 require'nvim-tree'.setup {
   disable_netrw        = true,
   hijack_netrw         = true,
-  open_on_setup        = false,
-  ignore_ft_on_setup   = {},
+  -- open_on_setup        = false,
+  -- ignore_ft_on_setup   = {},
   auto_reload_on_write = true,
   open_on_tab          = false,
   hijack_cursor        = true,
   update_cwd           = false,
-  update_to_buf_dir    = {
-    enable = true,
-    auto_open = true,
-  },
+  -- update_to_buf_dir    = {
+  --   enable = true,
+  --   auto_open = true,
+  -- },
   renderer = {
     highlight_opened_files = 'all',
   },
@@ -43,14 +43,14 @@ require'nvim-tree'.setup {
   },
   view = {
     width = 50,
-    height = 30,
-    hide_root_folder = false,
+    -- height = 30,
+    -- hide_root_folder = false,
     side = 'left',
-    auto_resize = false,
-    mappings = {
-      custom_only = false,
-      list = {}
-    },
+    -- auto_resize = false,
+    -- mappings = {
+    --   custom_only = false,
+    --   list = {}
+    -- },
     number = false,
     relativenumber = false,
     signcolumn = "yes"
